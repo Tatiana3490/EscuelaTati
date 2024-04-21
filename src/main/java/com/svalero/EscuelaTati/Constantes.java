1c4066412256cf7b878254edaba1d32d8b6b7464
TatianaAM <a27756@svalero.com>
2024-04-21 20:39:33 +0200

    añadios estructura del proyecto con carpetas y archivos que se van a utilizar y añadimos librerias en el pom

--- src/main/java/com/svalero/EscuelaTati/Constantes.java
+++ src/main/java/com/svalero/EscuelaTati/Constantes.java
@@ -1,6 +1,11 @@
 package com.svalero.EscuelaTati;
 
 public class Constantes {
-    public static final String DRIVER ="";
-    public static final String CONNECTION
+
+    public static final String DATABASE ="##Talcubilla";
+    public static final String USERNAME ="##Talcubilla";
+    public static final String PASSWORD ="T1990a4M3";
+
+    public static final String DRIVER ="oracle.jdbc.driver.OracleDriver";
+    public static final String CONNECTION_STRING ="jdbc:oracle:thin:@//localhost:1521/xe" + DATABASE;
 }
--- src/main/java/com/svalero/EscuelaTati/Main.java
+++ src/main/java/com/svalero/EscuelaTati/Main.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import static com.svalero.EscuelaTati.Constantes.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola");
@@ -11,10 +13,8 @@ public class Main {
         Connection conecction;
 
         try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            conecction = DriverManager.getConnection(
-                    "jdbc:oracle:thin:@//localhost:1521/xe",
-                    "##Talcubilla", "T1990a4M3");
+            Class.forName(DRIVER);
+            conecction = DriverManager.getConnection(CONNECTION_STRING, USERNAME, PASSWORD);
             System.out.println("Se ha conectado con la base de datos");
         } catch (ClassNotFoundException cnfe) {
             cnfe.printStackTrace();
